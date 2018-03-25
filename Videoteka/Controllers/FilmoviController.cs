2150fc0d333263d23b249952edfe0e3df82538b5
Josip <Josip@DESKTOP-CSNSJKM>
2018-03-25 22:58:40 +0200

    Pocetak rada na aplikaciji.

---
+++ Videoteka/Controllers/FilmoviController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Videoteka.Models;
+
+namespace Videoteka.Controllers
+{
+    public class FilmoviController : Controller
+    {
+        // GET: Filmovi/Random
+        public ActionResult Random()
+        {
+            var film = new Film() { Naziv = "Spiderman" };
+            return View(film);
+        }
+    }
+}
